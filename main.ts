adae53d5b9b08c2051766bc8a3d39933f02a36ca
kimts2627 <kimtstststs@gmail.com>
2021-02-21 17:23:51 +0900

    Chapter 1 complete

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
 console.log('Hello world');
+
+// type SuccessState = {
+//   response: {
+//     body: string;
+//   };
+// };
+// type FailState = {
+//   reason: string;
+// };
+// type LoginState = SuccessState | FailState;
+
+// function pprintLoginState(state: LoginState): void {
+
+//   if('response' in state) {
+//     console.log(state.response.body);
+//   }
+//   else {
+//     console.log(state.reason);
+//   }
+// }
+
+// Discriminated Union Case **************************************
+
+type SuccessState = {
+  result: 'success',
+  response: {
+    body: string
+  }
+}
+type FailState = {
+  result: 'fail',
+  reason: string
+}
+type LoginState = SuccessState | FailState;
+
+function pprintLoginState(state: LoginState): void {
+
+  if(state.result === 'success') {
+    console.log(state.response.body);
+  }
+  else {
+    console.log(state.reason);
+  }
+}
